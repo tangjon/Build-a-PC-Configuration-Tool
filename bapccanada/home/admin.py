369c7601dace42bf43431d6053a6cbe6f8bd6790
Gary Gao <ubcgary@gmail.com>
2018-05-06 15:40:58 -0700

    -Fix inheritance issue, allowing makemigrations to run -Use django-polymorphic to address GenericKeyField challenges -added in slugs for future url use

--- bapccanada/home/admin.py
+++ bapccanada/home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from home.models import GPU, Review, Build, UserProfile
 
 # Register your models here.
+admin.site.register(GPU)
+admin.site.register(Review)
+admin.site.register(Build)
+admin.site.register(UserProfile)
