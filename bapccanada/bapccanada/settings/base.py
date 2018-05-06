96d4549757bafec29ad7504911e73e7248907237
Jonathan Tang <tangjon@hotmail.com>
2018-05-06 16:21:15 -0700

    base url - Change url patterns of {} to [] - configure existing namespaces
    add profile template
    - simple ui
    
    add login template
    - dummy login page

--- bapccanada/bapccanada/settings/base.py
+++ bapccanada/bapccanada/settings/base.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home.apps.HomeConfig'
+    'home.apps.HomeConfig',
+    'account.apps.AccountConfig'
 ]
 
 MIDDLEWARE = [
