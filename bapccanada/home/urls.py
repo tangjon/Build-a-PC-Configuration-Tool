b9530ed419391a7c93cd639a9a77b6d6996f9427
Jonathan Tang <tangjon@hotmail.com>
2018-05-06 18:29:08 -0700

    use auth form for login

--- bapccanada/home/urls.py
+++ bapccanada/home/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = 'home'
 urlpatterns = [
-    re_path(r'^$', views.index, name='index'),
-    path('login/', views.login, name="login")
+    re_path(r'^$', views.home_view, name='home'),
+    path('login/', views.login_view, name="login")
 ]
