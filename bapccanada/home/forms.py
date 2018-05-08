7862d2cef556516db8703b1a81063a9eb7fe3cfb
Jonathan Tang <tangjon@hotmail.com>
2018-05-07 22:48:11 -0700

    ignore sqlite3
    Implement add users form and change url names

---
+++ bapccanada/home/forms.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super(SignUpForm, self).save(commit=False)
+
+        if commit:
+            user.save()
--- bapccanada/home/views.py
+++ bapccanada/home/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+
+from .forms import SignUpForm
 
 
 def home_view(request):
@@ -15,3 +18,16 @@ def login_view(request):
     else:
         form = AuthenticationForm()
     return render(request, 'login.html', {'title': 'Login', 'form': form})
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = SignUpForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            form.save();
+            return redirect('home:signup')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
