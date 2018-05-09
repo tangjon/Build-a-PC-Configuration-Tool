7d4321f6f4815a020e337399abf58a43ebdba338
Gary Gao <ubcgary@gmail.com>
2018-05-08 21:46:34 -0700

    -Add Build app backbone structure -Update .gitignore to allow build app to be committed

--- bapccanada/build/views.py
+++ bapccanada/build/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 
 def create(request):
-    return render(request, 'create.html', {'title': 'Profile', 'slug': 'user'})
+    return render(request, 'create.html', {'title': 'Start a build', 'slug': 'user'})
 # Create your views here.
