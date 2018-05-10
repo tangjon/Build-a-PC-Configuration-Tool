e4265280cf719d5108c0c64e4b519789cf9b0b06
Jonathan Tang <tangjon@hotmail.com>
2018-05-10 00:26:18 -0700

    idk anymore

--- bapccanada/account/forms.py
+++ bapccanada/account/forms.py
@@ -1,26 +1,27 @@
 from django import forms
 from django.contrib.auth import password_validation
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UsernameField
 
 from django.contrib.auth.models import User
+from django.contrib.auth.validators import ASCIIUsernameValidator
 from django.forms import ModelForm
 
 from account.models import UserProfile
 
 
 class SignUpForm(UserCreationForm):
-    email1 = forms.EmailField(
-        label="Email",
-        help_text="Email Address",
-    )
-    email2 = forms.EmailField(
-        label="Email Confirmation",
-        help_text="Enter the same email as before, for verification.",
-    )
+    # email1 = forms.EmailField(
+    #     label="Email",
+    #     help_text="Email Address",
+    # )
+    # email2 = forms.EmailField(
+    #     label="Email Confirmation",
+    #     help_text="Enter the same email as before, for verification.",
+    # )
 
     class Meta:
         model = UserProfile
-        fields = ['email1', 'email2']
+        fields = ['username', 'email', 'password1', 'password2']
     #
     # def save(self, commit=True):
     #     user = super().save(commit=False)
--- bapccanada/account/models.py
+++ bapccanada/account/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 
 
@@ -6,7 +7,18 @@ from django.db import models
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     email = models.EmailField(blank=False, unique=True)
-    USERNAME_FIELD = 'email'
+    username_validator = UnicodeUsernameValidator()
+    username = models.CharField(
+        'username',
+        max_length=150,
+        unique=True,
+        help_text="Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.",
+        validators=[username_validator],
+        error_messages={
+            'unique': "A user with that username already exists.",
+        },
+    )
+    USERNAME_FIELD = 'username'
 
     def __str__(self):
         return self.user.name
