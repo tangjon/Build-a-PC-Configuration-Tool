68c653c9a55f677beeba713e229d0c7d124fb08b
Jonathan Tang <tangjon@hotmail.com>
2018-05-09 19:30:05 -0700

    Move userprofile to account - add email field

--- bapccanada/account/models.py
+++ bapccanada/account/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(blank=False, unique=True)
+    # birth_date = models.DateField(help_text="YYYY-MM-DD")
+
+    def __str__(self):
+        return self.user.name
