64afc4ffde6f660dd34f0faecf188d6611b9d9fc
Jonathan Tang <tangjon@hotmail.com>
2018-05-10 00:06:23 -0700

    forms stable for now

--- bapccanada/account/forms.py
+++ bapccanada/account/forms.py
@@ -1,6 +1,9 @@
 from django import forms
+from django.contrib.auth import password_validation
 from django.contrib.auth.forms import UserCreationForm
+
 from django.contrib.auth.models import User
+from django.forms import ModelForm
 
 from account.models import UserProfile
 
--- bapccanada/account/models.py
+++ bapccanada/account/models.py
@@ -5,7 +5,8 @@ from django.db import models
 # Create your models here.
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # email = models.EmailField(blank=False, unique=True)
+    email = models.EmailField(blank=False, unique=True)
+    USERNAME_FIELD = 'email'
 
     def __str__(self):
         return self.user.name
