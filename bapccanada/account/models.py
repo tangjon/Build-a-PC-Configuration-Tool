82ac75c16160904a8972549507a5ee420018459c
Jonathan Tang <tangjon@hotmail.com>
2018-05-10 22:47:52 -0700

    fix syntax error

--- bapccanada/account/models.py
+++ bapccanada/account/models.py
@@ -6,19 +6,12 @@ from django.db import models
 # Create your models here.
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    email = models.EmailField(blank=False, unique=True)
-    # username_validator = UnicodeUsernameValidator()
-    # username = models.CharField(
-    #     'username',
-    #     max_length=150,
-    #     unique=True,
-    #     help_text="Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.",
-    #     validators=[username_validator],
-    #     error_messages={
-    #         'unique': "A user with that username already exists.",
-    #     },
-    # )
-    # USERNAME_FIELD = 'username'
+    birth_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
-        return self.user.name
+        return self.user.username
+
+
+class UserTest(User):
+    age = models.IntegerField()
+    birth_date = models.DateField()
