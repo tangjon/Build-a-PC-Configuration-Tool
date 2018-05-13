1c796100776e467ed162f2b485efacb835cbb90e
Jonathan Tang <tangjon@hotmail.com>
2018-05-13 11:51:52 -0700

    clean up - remove useless fluff

--- bapccanada/account/models.py
+++ bapccanada/account/models.py
@@ -7,31 +7,9 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
-# class UserProfile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     birth_date = models.DateField(auto_now_add=True)
-#
-#     def __str__(self):
-#         return self.user.username
-
-
-#
-# @receiver(post_save, sender=User)
-# def create_or_update_user_profile(sender, instance, created, **kwargs):
-#     if created:m
-#         UserProfile.objects.create(user=instance)
-#     instance.profile.save()
-
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    birth_date = models.DateField(null=True, blank=True)
+    birth_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.user.username
-
-
-# @receiver(post_save, sender=User)
-# def create_or_update_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         UserProfile.objects.create(user=instance)
-#     instance.userprofile.save()
