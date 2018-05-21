e16a6b2c24a0458a5f8e33979aa73848a23eb692
Jonathan Tang <tangjon@hotmail.com>
2018-05-21 15:06:14 -0700

    configure urls for user settings - example: /user/username/saved

--- bapccanada/user/views.py
+++ bapccanada/user/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.views.generic import TemplateView
+
+
 def view_profile(request):
     return render(request, 'profile.html', {'title': 'Profile', 'slug': 'user'})
 
@@ -27,3 +31,57 @@ def view_security(request):
 
 def view_saved(request):
     return render(request, 'saved.html', {'title': 'Preferences', 'slug': 'saved'})
+
+
+class ProfileView(TemplateView):
+    template_name = 'profile.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Profile'
+        return context
+
+
+class PreferencesView(TemplateView):
+    template_name = 'preferences.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Preferences'
+        return context
+
+
+class CommentsView(TemplateView):
+    template_name = 'comments.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Comments'
+        return context
+
+
+class BuildsView(TemplateView):
+    template_name = 'builds.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Builds'
+        return context
+
+
+class SecurityView(TemplateView):
+    template_name = 'security.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Security'
+        return context
+
+
+class SavedView(TemplateView):
+    template_name = 'saved.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Saved Parts'
+        return context
