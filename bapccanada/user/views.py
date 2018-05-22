3d3545b81a22de59d3c26d65f3b6671708e6c63c
Jonathan Tang <tangjon@hotmail.com>
2018-05-21 19:10:04 -0700

    Profile views show based on user, existing and non-existing

--- bapccanada/user/views.py
+++ bapccanada/user/views.py
@@ -1,26 +1,36 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
-
+from django.shortcuts import get_object_or_404
 # Create your views here.
 from django.urls import reverse
 from django.views.generic import TemplateView
+from django.contrib.auth.models import User
 
 
 class BaseProfileView(TemplateView):
     title_name = None
+    browse_user = None
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = self.title_name
+        context['browse_user'] = self.browse_user
         return context
 
     def dispatch(self, request, *args, **kwargs):
-        # Todo WA: reverse by template name
-        if request.path != reverse('user:' + self.template_name.split('.html')[0], kwargs={
-            'username': request.user.username
-        }):
-            return redirect('home:home')
+        self.browse_user = get_object_or_404(User, username=kwargs['username'])
         return super(BaseProfileView, self).dispatch(request, *args, **kwargs)
+
+        # Todo WA: reverse by template name
+        # # Check if this is the current auth user's profile
+        # if request.path == reverse('user:' + self.template_name.split('.html')[0], kwargs={
+        #     'username': request.user.username
+        # }):
+        #     return super(BaseProfileView, self).dispatch(request, *args, **kwargs)
+        # else:
+        #     # Check whether user exist on database
+        #     self.browse_user = get_object_or_404(User, username=kwargs['username'])
+        #     return super(BaseProfileView, self).dispatch(request, *args, **kwargs)
 
 
 class ProfileView(BaseProfileView):
