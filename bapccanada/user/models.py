5fe0558054d203cf256a4e8f6fab221d971f5a21
Jonathan Tang <tangjon@hotmail.com>
2018-05-23 22:45:04 -0700

    Install pillow for image field
    Add UserProfile members
    - avatar
    - biography

--- bapccanada/user/models.py
+++ bapccanada/user/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     birth_date = models.DateField(null=True, blank=False)
+    # extra
+    biography = models.TextField(blank=True, max_length=300)
+    avatar = models.ImageField(height_field=60, width_field=60, default='/')
 
     def __str__(self):
         return self.user.username
