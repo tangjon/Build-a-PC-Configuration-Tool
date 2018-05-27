05839eaf96e65255721ae55b4083810eeb38bfe6
Jonathan Tang <tangjon@hotmail.com>
2018-05-27 12:15:58 -0700

    register user preferences

--- bapccanada/user/admin.py
+++ bapccanada/user/admin.py
@@ -4,29 +4,47 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
-from user.models import UserProfile, UserSettings
+from user.models import UserProfile, UserPreferences
+
+admin.site.register(UserProfile)
+admin.site.register(UserPreferences)
 
 
-class UserProfileInline(admin.StackedInline):
-    model = UserProfile
-    verbose_name_plural = 'Profile'
-    fk_name = 'user'
 
+# COMBINE EVERYTHING USERPROFILE RELATED
 
-class UserSettingsInline(admin.StackedInline):
-    model = UserSettings
-    verbose_name_plural = 'Settings'
-    fk_name = 'user'
+# class UserPreferencesInline(admin.StackedInline):
+#     model = UserPreferences
+#     verbose_name_plural = 'Preferences'
+#     fk_name = 'profile'
+#
+#
+# class UserProfileAdmin(admin.ModelAdmin):
+#     inlines = (UserPreferencesInline,)
+#
+#     def get_inline_instances(self, request, obj=None):
+#         if not obj:
+#             return list()
+#         return super(UserProfileAdmin, self).get_inline_instances(request, obj)
+#
+#
+# admin.site.register(UserProfile, UserProfileAdmin)
 
+# COMBINE USER AND USERPROFILE
 
-class CustomUserAdmin(UserAdmin):
-    inlines = (UserProfileInline, UserSettingsInline)
+# class UserProfileInline(admin.StackedInline):
+#     model = UserProfile
+#     verbose_name_plural = 'Profile'
+#     fk_name = 'user'
+#
+#
+# class CustomUserAdmin(UserAdmin):
+#     inlines = (UserProfileInline,)
+#
+#     def get_inline_instances(self, request, obj=None):
+#         if not obj:
+#             return list()
+#         return super(CustomUserAdmin, self).get_inline_instances(request, obj)
 
-    def get_inline_instances(self, request, obj=None):
-        if not obj:
-            return list()
-        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
-
-
-admin.site.unregister(User)
-admin.site.register(User, CustomUserAdmin)
+# admin.site.unregister(User)
+# admin.site.register(User, CustomUserAdmin)
