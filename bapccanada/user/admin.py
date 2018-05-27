c4a95b81cf7eaf6e621c54ac03af37d3197f58b8
Jonathan Tang <tangjon@hotmail.com>
2018-05-26 20:30:56 -0700

    UserSettings points to User

--- bapccanada/user/admin.py
+++ bapccanada/user/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+from user.models import UserProfile, UserSettings
+
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+
+
+class UserSettingsInline(admin.StackedInline):
+    model = UserSettings
+    verbose_name_plural = 'Settings'
+    fk_name = 'user'
+
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (UserProfileInline, UserSettingsInline)
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
--- bapccanada/user/models.py
+++ bapccanada/user/models.py
@@ -3,18 +3,25 @@ from django.db import models
 from django.conf import settings
 
 
-class UserSettings(models.Model):
-    OPEN_NEW_LINKS = models.BooleanField(default=False, blank=True)
-    OPEN_NEW_LINKS2 = models.BooleanField(default=False, blank=True)
-    OPEN_NEW_LINKS3 = models.BooleanField(default=False, blank=True)
-
-
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     birth_date = models.DateField(null=True, blank=False)
     # extra
     biography = models.TextField(blank=True, max_length=300)
     avatar = models.ImageField(upload_to='profile_image', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class UserSettings(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True
+    )
+    serves_hot_dogs = models.BooleanField(default=False)
+    serves_pizza = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
