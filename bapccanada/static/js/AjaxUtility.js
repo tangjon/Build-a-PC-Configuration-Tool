f846c87fdcb095503ef1136be704259dd6cb542b
Jonathan Tang <tangjon@hotmail.com>
2018-06-06 22:41:32 -0700

    fix jonathan's mistake

--- bapccanada/static/js/AjaxUtility.js
+++ bapccanada/static/js/AjaxUtility.js
@@ -3,8 +3,6 @@ export default function doAjaxPost(oData, sUrl, fnSuccess, fnError) {
         type: "POST",
         url: sUrl,
         data: oData,
-        success: fnSuccess,
-        error: fnError,
         dataType: "json",
         success: fnSuccess,
         error: (fnError && typeof fnError === "function") ? fnError : function (error) {
