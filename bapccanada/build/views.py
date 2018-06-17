30b1fa2298bc85c824b2cca13f62246f3e1a1399
Jonathan Tang <tangjon@hotmail.com>
2018-06-17 15:49:16 -0700

    moving shortcode code

--- bapccanada/build/views.py
+++ bapccanada/build/views.py
@@ -12,7 +12,22 @@ class Create(View):
         return render(request, 'create.html', {'title': self.title, 'build': self.build,
                                                'component_list': self.component_list})
 
-    def get(self, request):
+    def get(self, request, *args, **kwargs):
+        if 'shortcode' in kwargs:
+            build = get_object_or_404(Build, shortcode=kwargs['shortcode'])
+            if request.user.is_authenticated:
+                currentBuild = CurrentBuild.objects.get(tracked_user=request.user.userprofile)
+                currentBuild.tracked_build = build
+                currentBuild.save()
+
+            else:
+                currentBuild = CurrentBuild.objects.all()[0]
+                currentBuild.tracked_build = build
+
+            print(currentBuild.tracked_user.user.username)
+            print(currentBuild.tracked_build.name)
+
+            currentBuild.save()
         self.title = "Current Part List"
         self.build = Build.handle_build_tracking(request)
         self.component_list = self.build.get_component_dict()
